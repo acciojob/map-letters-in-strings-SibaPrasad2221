8e3ee21c3f5cf38759c5189c8aee7637b0800a09
~egoist <sahoosibaprasad2221@gmail.com>
2024-03-12 16:35:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const mapString = str => {
+   const map = {};
+   for(let i = 0; i < str.length; i++){
+      if(map.hasOwnProperty(str[i])){
+         map[str[i]] = map[str[i]].concat(i);
+      }else{
+         map[str[i]] = [i];
+      };
+   };
+   return map;
+};
